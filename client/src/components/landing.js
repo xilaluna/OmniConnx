ec52f16984f5b4c00ef78d39da1b0c6bd8e683ae
Chaoneng Tan <chaonengtan@gmail.com>
2021-09-10 12:24:13 -0700

    +landingPage
    built landing page as on framework

--- client/src/components/landing.js
+++ client/src/components/landing.js
@@ -4,7 +4,53 @@ import './landing.css'
 function Landing() {
   return (
     <div className='landing'>
-      This is the landing page
+        <div className='hero'>
+            <div>
+                <h1>Welcome to Omniconnx</h1>
+                <p>some super cringy slogan</p>
+                <button>Register</button>
+            </div>
+            <img src='https://via.placeholder.com/400'></img>
+        </div>
+        <div className='blurbs'>
+            <div className='blurb'>
+                <img src='https://via.placeholder.com/100'></img>
+                <h4>awesome title</h4>
+                <p>description</p>
+            </div>
+            <div className='blurb'>
+                <img src='https://via.placeholder.com/100'></img>
+                <h4>awesome title</h4>
+                <p>description</p>
+            </div>
+            <div className='blurb'>
+                <img src='https://via.placeholder.com/100'></img>
+                <h4>awesome title</h4>
+                <p>description</p>
+            </div>
+        </div>
+        <div className='hero'>
+            <img src='https://via.placeholder.com/400'></img>
+            <div>
+                <h1>Something Cool</h1>
+                <p>some cringy advertisement</p>
+                <button>Button!</button>
+            </div>
+        </div>
+        <div className='cards'>
+            <div className='card'>
+                <img src='https://via.placeholder.com/100'></img>
+                <p>description</p>
+            </div>
+            <div className='card'>
+                <img src='https://via.placeholder.com/100'></img>
+                <p>description</p>
+            </div>
+            <div className='card'>
+                <img src='https://via.placeholder.com/100'></img>
+                <p>description</p>
+            </div>
+        </div>
     </div>
   )
 }
