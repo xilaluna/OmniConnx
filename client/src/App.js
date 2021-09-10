7a5b822e818c2a5ac29c60f65fe936a6cf3351dd
Chaoneng Tan <chaonengtan@gmail.com>
2021-09-10 11:47:26 -0700

    +navbar, +footer, +landing

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { HashRouter as Router, Route } from 'react-router-dom'
+import Navbar from './components/navbar'
+import Footer from './components/footer'
+import Landing from './components/landing'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Route exact path="/" component={Landing}/>
+        {/* <Route path="/" component={} /> */}
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ client/src/components/landing.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './landing.css'
+
+function Landing() {
+  return (
+    <div className='landing'>
+      This is the landing page
+    </div>
+  )
+}
+
+export default Landing
