afbda8f3ee2ead3e259156865ead969e94561ada
Jose <hose153@gmail.com>
2021-09-14 15:59:26 -0500

    Created routes

--- server/index.js
+++ server/index.js
@@ -39,3 +39,104 @@ app.listen(port, () => {
   
   module.exports = app;
   
+
+/* PASSPORT LOCAL AUTHENTICATION */
+
+passport.use(UserDetails.createStrategy());
+
+passport.serializeUser(UserDetails.serializeUser());
+passport.deserializeUser(UserDetails.deserializeUser());
+
+/* ROUTES */
+
+//Landing Page
+app.get('/',
+  (req, res) => res.sendFile('html/index.html', {root: __dirname})
+);
+
+//Create user
+app.post('/signup', (req, res) => {
+  var user = new User(req.body);
+
+  user.save()
+    .then((user) => {
+      res.redirect(`/profile/${user._id}`);
+    })
+    .catch((err) => {
+      // Handle Errors
+    }) ;
+});
+
+//Get all the posts
+app.get('/all_post',
+  (req, res) => res.sendFile('html/allPost.html', {root: __dirname})
+);
+
+
+/* AUTH REQUIRES LOGIN*/
+const connectEnsureLogin = require('connect-ensure-login');
+
+//Login page logic
+app.post('/login', (req, res, next) => {
+  passport.authenticate('local',
+  (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+
+    if (!user) {
+      return res.redirect('/login?info=' + info);
+    }
+
+    req.logIn(user, function(err) {
+      if (err) {
+        return next(err);
+      }
+
+      return res.redirect('/');
+    });
+
+  })(req, res, next);
+});
+
+//Login page
+app.get('/login',
+  (req, res) => res.sendFile('html/login.html',
+  { root: __dirname })
+);
+
+//Display profile
+app.get('/profile/:userId',
+  connectEnsureLogin.ensureLoggedIn(),
+  (req, res) => res.send({user: req.user})
+);
+
+//Edit profile
+app.get('/profile/:userId',
+  connectEnsureLogin.ensureLoggedIn(),
+  (req, res) => res.send({user: req.user})
+);
+
+//Create post
+app.get('/create_post',
+  connectEnsureLogin.ensureLoggedIn(),
+  (req, res) => res.send({user: req.user})
+);
+
+//Edit post
+app.get('/edit_post/:postId',
+  connectEnsureLogin.ensureLoggedIn(),
+  (req, res) => res.send({user: req.user})
+);
+
+app.post('/edit_post/:postId',
+  connectEnsureLogin.ensureLoggedIn(),
+  (req, res) => res.send({user: req.user})
+);
+
+//Delete post
+app.delete('/delete_post',
+  connectEnsureLogin.ensureLoggedIn(),
+  (req, res) => res.send({user: req.user})
+);
+
