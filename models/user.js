515c301f43b55c3e748e73852fae0029be2337d4
Aetrix27 <davedogg323@gmail.com>
2021-09-14 11:28:58 -0700

    initialized user database model and index.js

---
+++ models/user.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    createdAt: { type: Date },
+    updatedAt: { type: Date },
+    password: { type: String, select: false },
+    username: { type: String, required: true },
+    cards : [{ type: Schema.Types.ObjectId, ref: "Card" }]
+
+},
+    { timestamps: { createdAt: 'created_at' } }
+);
+
+//authentication
+// Must use function here! ES6 => functions do not bind this!
+UserSchema.pre("save", function (next) {
+    // ENCRYPT PASSWORD
+    const user = this;
+    if (!user.isModified("password")) {
+        return next();
+    }
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(user.password, salt, (err, hash) => {
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+// Need to use function to enable this.password to work.
+UserSchema.methods.comparePassword = function (password, done) {
+    bcrypt.compare(password, this.password, (err, isMatch) => {
+        done(err, isMatch);
+    });
+};
+
+module.exports = mongoose.model("User", UserSchema);
--- server/index.js
+++ server/index.js
@@ -1,1 +1,41 @@
 const test = "test"
+const path = require("path");
+const express = require('express');
+var methodOverride = require('method-override');
+var cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+const app = express();
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+//const exphbs  = require('express-handlebars');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+require('./data/ezstudy-db');
+port = 3000
+
+app.use(expressValidator());
+app.use(cookieParser());
+app.use(methodOverride('_method'));
+
+/*
+app.engine('handlebars', exphbs({
+  layoutsDir: __dirname + '/views/layouts',
+  defaultLayout: 'main'
+}));
+*/
+
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'handlebars');
+
+
+//require('./controllers/auth.js')(app);
+// Start Server
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
+    
+  
+  module.exports = app;
+  
