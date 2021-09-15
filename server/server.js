5998620b13023a8b035cb3796a94da0f5173ab91
Xila Luna <xilaluna2@gmail.com>
2021-09-15 16:47:29 -0700

    [update]reinit node

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
-var express = require('express')
-var app = express()
+// var express = require('express')
+// var app = express()
 
-//Define request response in root URL (/)
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+// //Define request response in root URL (/)
+// app.get('/', function (req, res) {
+//   res.send('Hello World')
+// })
 
-//Launch listening server on port 5000
-app.listen(8080, function () {
-  console.log('App listening on port 5000!')
-})
+// //Launch listening server on port 5000
+// app.listen(8080, function () {
+//   console.log('App listening on port 5000!')
+// })
--- server/tests/server-tests.js
+++ server/tests/server-tests.js
@@ -1,6 +1,6 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const server = require('./server/index.js');
-const should = chai.should()
+// const chai = require('chai');
+// const chaiHttp = require('chai-http');
+// const server = require('./server/index.js');
+// const should = chai.should()
 
-chai.use(chaiHttp);
+// chai.use(chaiHttp);
