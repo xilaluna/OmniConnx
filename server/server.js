d714c58d76e58c795786a58ac4727dde0cebb760
David Guerrero <davedogg323@gmail.com>
2021-09-16 20:26:12 -0700

    fixed server.js file

--- server/server.js
+++ server/server.js
@@ -1,12 +1,28 @@
-// var express = require('express')
-// var app = express()
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-// //Define request response in root URL (/)
-// app.get('/', function (req, res) {
-//   res.send('Hello World')
-// })
+const app = express();
 
-// //Launch listening server on port 5000
-// app.listen(8080, function () {
-//   console.log('App listening on port 5000!')
-// })
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Test" });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
