acddbcb156aa238271a094b69f3174faa51bc697
Xila Luna <xilaluna2@gmail.com>
2021-09-20 23:14:01 -0700

    [add] bootstrap import

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,22 @@
-import './App.css';
-import { HashRouter as Router, Route } from 'react-router-dom'
-import Navbar from './components/navbar'
-import Footer from './components/footer'
-import Landing from './components/landing'
-import UserProfile from './components/userProfile'
+import "./App.css"
+import { HashRouter as Router, Route } from "react-router-dom"
+import Navbar from "./components/navbar"
+import Footer from "./components/footer"
+import Landing from "./components/landing"
+import UserProfile from "./components/userProfile"
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Navbar />
-        <Route exact path="/" component={Landing}/>
+        <Route exact path="/" component={Landing} />
         <Route path="/prof" component={UserProfile} />
         <Footer />
       </div>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
