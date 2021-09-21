67ceefea109f5aebd1bf51d253f116de56b757ef
David Guerrero <davedogg323@gmail.com>
2021-09-20 21:12:32 -0700

    Fixed import errors and mistakes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const db = require("./server/models");
+const db = require("./server/models/user");
 
 db.mongoose
   .connect(db.url, {
--- server/models/user.js
+++ server/models/user.js
@@ -1,59 +1,69 @@
 const mongoose = require("mongoose")
 const bcrypt = require("bcryptjs")
-const Schema = mongoose.Schema
+const dbConfig = require("../data/omniconnx-db.js");
+
+//const Schema = mongoose.Schema
 
 const db = {};
 db.mongoose = mongoose
 db.url = dbConfig.url;
-db.tutorials = require("./tutorial.model.js")(mongoose);
+//db.tutorials = require("./tutorial.model.js")(mongoose);
+module.exports = mongoose => {
 
-const UserSchema = new Schema(
-  {
-    password: { type: String, select: false },
-    username: { type: String, required: true },
-    first_name: { type: String, required: true },
-    last_name: { type: String, required: true },
-    phone: { type: String, required: true },
-    location_value: { type: String, required: true },
-    account_type: { type: String, required: true },
-    company_id: { type: Int, required: true },
-    calendar_id: { type: Int, required: true },
-    CV_ID: { type: Int, required: true },
-    users: [{ type: Schema.Types.ObjectId, ref: "User" }],
-  },
-  { timestamps: { createdAt: "created_at" } }
-)
+  const UserSchema = mongoose.Schema(
+    {
+      password: { type: String, select: false },
+      username: { type: String, required: true },
+      first_name: { type: String, required: true },
+      last_name: { type: String, required: true },
+      phone: { type: String, required: true },
+      location_value: { type: String, required: true },
+      account_type: { type: String, required: true },
+      company_id: { type: Number, required: true },
+      calendar_id: { type: String, required: true },
+      CV_ID: { type: Number, required: true },
+      users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    },
+    { timestamps: { createdAt: "created_at" } }
+    
+  );
 
-//If you use this app with a front-end that needs id field instead of _id, you have to override toJSON 
-//method that map default object to a custom object. So the Mongoose model could be modified as following code:
-schema.method("toJSON", function() {
-  const { __v, _id, ...object } = this.toObject();
-  object.id = _id;
-  return object;
-});
+  /*
+  //If you use this app with a front-end that needs id field instead of _id, you have to override toJSON 
+  //method that map default object to a custom object. So the Mongoose model could be modified as following code:
+  schema.method("toJSON", function() {
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+  });
 
 
-//authentication
-// Must use function here! ES6 => functions do not bind this!
-UserSchema.pre("save", function (next) {
-  // ENCRYPT PASSWORD
-  const user = this
-  if (!user.isModified("password")) {
-    return next()
-  }
-  bcrypt.genSalt(10, (err, salt) => {
-    bcrypt.hash(user.password, salt, (err, hash) => {
-      user.password = hash
-      next()
+  //authentication
+  // Must use function here! ES6 => functions do not bind this!
+  UserSchema.pre("save", function (next) {
+    // ENCRYPT PASSWORD
+    const user = this
+    if (!user.isModified("password")) {
+      return next()
+    }
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash
+        next()
+      })
     })
   })
-})
 
-// Need to use function to enable this.password to work.
-UserSchema.methods.comparePassword = function (password, done) {
-  bcrypt.compare(password, this.password, (err, isMatch) => {
-    done(err, isMatch)
-  })
-}
+  // Need to use function to enable this.password to work.
+  UserSchema.methods.comparePassword = function (password, done) {
+    bcrypt.compare(password, this.password, (err, isMatch) => {
+      done(err, isMatch)
+    })
+  }
+  */
 
-module.exports = mongoose.model("User", UserSchema)
+  //module.exports = mongoose.model("User", UserSchema)
+  const UserClass = mongoose.model("User", UserSchema);
+  return UserClass
+};
+//module.exports = db;
