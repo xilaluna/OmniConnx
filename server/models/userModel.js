043792bc6364185f9d750542acbd0f3114adfb1f
John Marcos <johnsaguay@gmail.com>
2021-09-23 11:05:18 -0700

    Relocated files

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -2,7 +2,7 @@ module.exports = mongoose => {
   const UserSchema = mongoose.Schema(
     {
        first_name: { type: String, required: true },
-       last_name: { type: String, required: true },
+      //  last_name: { type: String, required: true },
       // password: { type: String, select: false },
       // username: { type: String, required: true },
       // phone: { type: String, required: true },
---
+++ server/server.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// simple route
+// app.get("/", (req, res) => {
+//   res.json({ message: "Test" });
+// });
+
+const db = require("./models/index");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
+
+
+  
+
+
+// app.post("/user", (req, res) => {
+//   try{
+//       const { first_name } = req.body;
+//   } catch (err) {
+//       console.log(err);
+//   }
+// });
+require("./routes/userRoutes")(app);
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
