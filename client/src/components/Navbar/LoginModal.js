90833eb552a6d748e7127514b45298da5fad2f61
Xila Luna <xilaluna2@gmail.com>
2021-09-22 22:16:39 -0700

    [update] login modal completed

--- client/src/components/Navbar/LoginModal.js
+++ client/src/components/Navbar/LoginModal.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import { Modal, Button, Nav } from "react-bootstrap"
+import { Modal, Button, Nav, Form } from "react-bootstrap"
 
 function LoginModal() {
   const [show, setShow] = useState(false)
@@ -12,17 +12,35 @@ function LoginModal() {
 
       <Modal show={show} onHide={handleClose} backdrop="static" keyboard={false}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal title</Modal.Title>
+          <Modal.Title>Log In</Modal.Title>
         </Modal.Header>
-        <Modal.Body>
-          I will not close if you click outside me. Don't even try to press escape key.
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary">Understood</Button>
-        </Modal.Footer>
+        <Form>
+          <Modal.Body>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="remember me" />
+            </Form.Group>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Modal.Footer>
+        </Form>
       </Modal>
     </>
   )
