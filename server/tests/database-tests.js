6652d1286b3cfb4f514b74d7a762ecae5741209a
Johnny Quinn <jftq97@gmail.com>
2021-09-27 06:54:20 -0400

    Added script for testing, installed mocha, wrote initial test code for database

--- server/tests/database-tests.js
+++ server/tests/database-tests.js
@@ -1,14 +1,34 @@
+//tests only for database
+
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../server');
-const User = require('../models/userModel')
-const should = chai.should()
+const User = require('../models/userModel');
+const { mongoose } = require('../models');
+const should = chai.should();
 
 // setting up test data for user model 
 const testUser = {
     "first_name": "test_user_first_name"
-}
+};
 
-describe('UserSchema', () => {
-    
+describe('User model tests', () => {
+//tests only for user model
+
+    //Establishing connection to local mongo database
+    before(function (done) {
+        mongoose.connect('mongodb://localhost/testDatabase');
+        const db = mongoose.connection;
+        db.on('error', console.error.bind(console, 'connection error'));
+        db.once('open', function() {
+            console.log('We are connected to test database!');
+            done();
+        });
+    });
+
+    after(function(done){
+        mongoose.connection.db.dropDatabase(function(){
+            mongoose.connection.close(done);
+        });
+    });
 })
