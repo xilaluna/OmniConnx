0d5be3f10d760e32b96b0d1cdba16611d20baa49
John Marcos <johnsaguay@gmail.com>
2021-09-27 22:24:51 -0700

    Added functional authentication and updated user contollers

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -3,28 +3,28 @@ module.exports = mongoose => {
 //Implement random id generation later
   const UserSchema = mongoose.Schema(
     {
-       first_name: { type: String, required: true },
-       last_name: { type: String, required: true },
-      // password: { type: String, select: false },
-      // username: { type: String, required: true },
+      //  first_name: { type: String, required: true },
+      //  last_name: { type: String, required: true },
+      username: { type: String, required: true },
+      passwordHash: { type: String, required: true },
       // phone: { type: String, required: true },
       // location_value: { type: String, required: true },
       // account_type: { type: String, required: true },
       // company_id: { type: Number, required: true },
       // calendar_id: { type: String, required: true },
       // CV_ID: { type: Number, required: true },
-       posts: [{ type: mongoose.Schema.Types.ObjectId, ref: "post" }],
+      //  posts: [{ type: mongoose.Schema.Types.ObjectId, ref: "post" }],
     },
     { timestamps: { createdAt: "created_at" } }
   
   );
   //If you use this app with a front-end that needs id field instead of _id, you have to override toJSON 
   //method that map default object to a custom object. So the Mongoose model could be modified as following code:
-  UserSchema.method("toJSON", function() {
-    const { __v, _id, ...object } = this.toObject();
-    object.id = _id;
-    return object;
-  });
+  // UserSchema.method("toJSON", function() {
+  //   const { __v, _id, ...object } = this.toObject();
+  //   object.id = _id;
+  //   return object;
+  // });
 
   /*
   //authentication
