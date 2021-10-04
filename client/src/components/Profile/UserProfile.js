249946113e9dd66f4737c213adc4698f69587231
Sreenvitha Chikati <srechi453.fusdk12@gmail.com>
2021-10-04 02:33:11 -0700

    Building html user profile structure

--- client/src/components/Profile/UserProfile.js
+++ client/src/components/Profile/UserProfile.js
@@ -1,10 +1,43 @@
 import React from 'react'
-// import './UserProfile.css'
+import './UserProfile.css'
 
 function UserProfile() {
   return (
     <div className='userProfile'>
-        This is user profile
+      {/* Main User Area */}
+      <div className="profmain">
+      <img src="https://via.placeholder.com/200"></img>
+        <div>
+          <h1>Username</h1>
+          <div className="profinfo">
+            <h4>Degree</h4>
+            <h4>Gender</h4>
+            <h4>Other Stuff</h4>
+          </div>
+        </div>
+      </div>
+      {/* Bio Area */}
+      <div className="bio">
+        <div className="biocard">
+          <p>Bio Area</p>
+        </div>
+      </div>
+      {/* Skills? Area */}
+      <div className="cards">
+        <div className="card">
+          <img src="https://via.placeholder.com/100"></img>
+          <p>description</p>
+        </div>
+        <div className="card">
+          <img src="https://via.placeholder.com/100"></img>
+          <p>description</p>
+        </div>
+        <div className="card">
+          <img src="https://via.placeholder.com/100"></img>
+          <p>description</p>
+        </div>
+      </div>
+
     </div>
   )
 }
