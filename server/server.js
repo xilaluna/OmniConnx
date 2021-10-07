e44a93fcc45e56a0392019cc33aa5a6289ed9329
Xila Luna <xilaluna2@gmail.com>
2021-10-07 10:34:05 -0700

    [add] build path

--- server/server.js
+++ server/server.js
@@ -1,39 +1,42 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
+const express = require("express")
+const dotenv = require("dotenv")
+const cookieParser = require("cookie-parser")
+const cors = require("cors")
 //const app = require('/index.js');
-const port = 8080;
+const port = 8080
 //"start": "concurrently \"react-scripts start\" \"cd backend && nodemon server\"",
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
+const app = express()
 
 var corsOptions = {
-  origin: "http://localhost:8081"
-};
+  origin: "http://localhost:8081",
+}
 
-app.use(cors(corsOptions));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
 
 //database
-const db = require("./models/index");
+const db = require("./models/index")
 db.mongoose
   .connect(db.url, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
   .then(() => {
-    console.log("Connected to the database!");
+    console.log("Connected to the database!")
   })
-  .catch(err => {
-    console.log("Cannot connect to the database!", err);
-    process.exit();
-  });
+  .catch((err) => {
+    console.log("Cannot connect to the database!", err)
+    process.exit()
+  })
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("../client/build"))
+}
 // app.post("/user", (req, res) => {
 //   try{
 //       const { first_name } = req.body;
@@ -42,14 +45,14 @@ db.mongoose
 //   }
 // });
 
-require("./routes/userRoutes")(app);
+require("./routes/userRoutes")(app)
 //require('./src/http-common')(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
+  console.log(`Server is running on port ${PORT}.`)
+})
 
 //require("./routes/postRoutes")(app);
 // set port, listen for requests
